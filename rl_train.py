ba7adccd25512828c1bae414a8c5254d87bc40eb
kof2001kop <kof2001kop@163.com>
2021-03-05 22:05:23 +0800

    c

--- rl_train.py
+++ rl_train.py
@@ -1,4 +1,27 @@
-import torch
+import numpy as np
+import gym
 
-c = torch.tensor([1, 2, 3, 4])
-print (c[1].item())
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.optimizers import Adam
+
+from rl.agents.dqn import DQNAgent
+from rl.policy import EpsGreedyQPolicy
+from rl.memory import SequentialMemory
+
+ENV_NAME = 'CartPole-v0'
+
+# Get the environment and extract the number of actions available in the Cartpole problem
+env = gym.make(ENV_NAME)
+np.random.seed(123)
+env.seed(123)
+nb_actions = env.action_space.n
+
+model = Sequential()
+model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(nb_actions))
+model.add(Activation('linear'))
+print(model.summary())
+
