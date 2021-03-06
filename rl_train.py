7a21c8226913e7c0fb4ffffa7fb90ee286c7faa1
kof2001kop <kof2001kop@163.com>
2021-03-06 15:21:00 +0800

    c

--- rl_train.py
+++ rl_train.py
@@ -1,35 +1,73 @@
-import numpy as np
+#创建环境
+
 import gym
+from gym import spaces
+import numpy as np
 
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten
-from keras.optimizers import Adam
+class Car2DEnv(gym.Env):
+    metadata = {
+        'render.modes': ['human', 'rgb_array'],
+        'video.frames_per_second': 2
+    }
 
-from rl.agents.dqn import DQNAgent
-from rl.policy import EpsGreedyQPolicy
-from rl.memory import SequentialMemory
+    def __init__(self):
+        self.xth = 0
+        self.target_x = 0
+        self.target_y = 0
+        self.L = 10
+        self.action_space = spaces.Discrete(5) # 0, 1, 2，3，4: 不动，上下左右
+        self.observation_space = spaces.Box(np.array([self.L, self.L]), np.array([self.L, self.L]))
+        self.state = None
 
-ENV_NAME = 'CartPole-v0'
+    def step(self, action):
+        assert self.action_space.contains(action), "%r (%s) invalid"%(action, type(action))
+        x, y = self.state
+        if action == 0:
+            x = x
+            y = y
+        if action == 1:
+            x = x
+            y = y + 1
+        if action == 2:
+            x = x
+            y = y - 1
+        if action == 3:
+            x = x - 1
+            y = y
+        if action == 4:
+            x = x + 1
+            y = y
+        self.state = np.array([x, y])
+        self.counts += 1
 
-# Get the environment and extract the number of actions available in the Cartpole problem
-env = gym.make(ENV_NAME)
-np.random.seed(123)
-env.seed(123)
-nb_actions = env.action_space.n
+        done = (np.abs(x)+np.abs(y) <= 1) or (np.abs(x)+np.abs(y) >= 2*self.L+1)
+        done = bool(done)
 
-model = Sequential()
-model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
-model.add(Dense(16))
-model.add(Activation('relu'))
-model.add(Dense(nb_actions))
-model.add(Activation('linear'))
-print(model.summary())
+        if not done:
+            reward = -0.1
+        else:
+            if np.abs(x)+np.abs(y) <= 1:
+                reward = 10
+            else:
+                reward = -50
 
-policy = EpsGreedyQPolicy()
-memory = SequentialMemory(limit=50000, window_length=1)
-dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10,
-target_model_update=1e-2, policy=policy)
-dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+        return self.state, reward, done, {}
 
-# Okay, now it's time to learn something! We visualize the training here for show, but this slows down training quite a lot. 
-dqn.fit(env, nb_steps=5000, visualize=True, verbose=2)
+    def reset(self):
+        self.state = np.ceil(np.random.rand(2)*2*self.L)-self.L
+        self.counts = 0
+        return self.state
+
+    def render(self, mode='human'):
+        return None
+
+    def close(self):
+        return None
+
+if __name__ == '__main__':
+    env = Car2DEnv()
+    env.reset()
+    env.step(env.action_space.sample())
+    print(env.state)
+    env.step(env.action_space.sample())
+    print(env.state)
