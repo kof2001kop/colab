ea9e9c0dca98b81a5954950e669909f8705572ff
kof2001kop <kof2001kop@163.com>
2021-03-09 16:36:00 +0800

    c

--- rl_train.py
+++ rl_train.py
@@ -2,84 +2,100 @@
 import gym
 from gym import spaces
 import numpy as np
+from keras.models import Sequential
 
-class Car2DEnv(gym.Env):
-    metadata = {
-        'render.modes': ['human', 'rgb_array'],
-        'video.frames_per_second': 2
-    }
+class kpr_stock(gym.Env):
+    
+    metadata = {'render.modes': ['human', 'ansi']}
+
 
     def __init__(self):
-        self.xth = 0
-        self.target_x = 0
-        self.target_y = 0
-        self.L = 10
-        self.action_space = spaces.Discrete(5) # 0, 1, 2，3，4: 不动，上下左右
-        self.observation_space = spaces.Box(np.array([self.L, self.L]), np.array([self.L, self.L]))
+        super(kpr_stock, self).__init__()
+        self.action_space = spaces.Discrete(3) # 0, 1, 2: 買入，賣出，觀望
+        self.observation_space = spaces.Box(np.array([0, 0]), np.array([10000000, 10]))
         self.state = None
-
+        self.init_money = 10000
+        self.cur_money = self.init_money
+        self.cur_stock_money = 0
+        self.t = 0
+        self.stock_data = self.load_stock_data()
+        self.pre_all_money = self.init_money
+        self.hold_stock_num = 0
+        
     def step(self, action):
         assert self.action_space.contains(action), "%r (%s) invalid"%(action, type(action))
-        x, y = self.state
-        if action == 0:
-            x = x
-            y = y
-        if action == 1:
-            x = x
-            y = y + 1
+        
+        if (self.t + 1 >= len(self.stock_data)) or (self.cur_money <= 0):
+            done = True
+            reward = 0
+            return self.state, reward, done, {}
+
+        #買入
+        if action == 0 and self.cur_money >= 100 * self.stock_data[self.t]:
+            self.cur_money -= 100 * self.stock_data[self.t]
+            self.cur_stock_money += 100 * self.stock_data[self.t]
+            self.hold_stock_num += 100
+            
+        #賣出
+        if action == 1 and self.hold_stock_num > 0:
+            self.cur_money += self.hold_stock_num * self.stock_data[self.t]
+            self.cur_stock_money = 0
+            self.hold_stock_num = 0
+        
+        #觀望
         if action == 2:
-            x = x
-            y = y - 1
-        if action == 3:
-            x = x - 1
-            y = y
-        if action == 4:
-            x = x + 1
-            y = y
-        self.state = np.array([x, y])
-        self.counts += 1
-
-        done = (np.abs(x)+np.abs(y) <= 1) or (np.abs(x)+np.abs(y) >= 2*self.L+1)
-        done = bool(done)
-
-        if not done:
-            reward = -0.1
+            pass
+                    
+                   
+        self.t += 1
+        
+        if self.cur_money + self.cur_stock_money - self.pre_all_money > 0:
+            reward = 10
+        elif self.cur_money + self.cur_stock_money - self.pre_all_money == 0:
+            reward = 0
         else:
-            if np.abs(x)+np.abs(y) <= 1:
-                reward = 10
-            else:
-                reward = -50
+            reward = -10
 
+        self.pre_all_money = self.cur_money + self.cur_stock_money
+        self.state = np.array([self.pre_all_money, self.stock_data[self.t]])
+        done = False
+        
         return self.state, reward, done, {}
 
+
     def reset(self):
-        self.state = np.ceil(np.random.rand(2)*2*self.L)-self.L
-        self.counts = 0
+        self.state = np.array([self.init_money, 0])
+        self.cur_money = self.init_money
+        self.cur_stock_money = 0
+        self.t = 0
+        self.pre_all_money = self.init_money
+        self.hold_stock_num = 0
         return self.state
 
+
+    def load_stock_data(self):
+        return np.array([9,8,7,6,5,4,3,2,1,9,8,7,6,5,4,3,2,1,9,8,7,6,5,4,3,2,1])
+    
+    
     def render(self, mode='human'):
         return None
+
 
     def close(self):
         return None
 
-if __name__ == '__main__':
-    env = Car2DEnv()
-    env.reset()
-    env.step(env.action_space.sample())
-    print(env.state)
-    env.step(env.action_space.sample())
-    print(env.state)
 
 #調用強化學習庫  
-#env = gym.make('Car2DEnv')
-for i_episode in range(20):
+env = kpr_stock()
+for i_episode in range(1000):
     observation = env.reset()
-    for t in range(100):
-        print(observation)
+    for t in range(30):
+        #print(observation)
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
+        #print(action, observation[0])
         if done:
-            print("Episode finished after {} timesteps".format(t+1))
+            print(observation[0])
             break
+
 env.close()
