d547d366f7bb37d8ce44b20697803f086b7d581f
kof2001kop <kof2001kop@163.com>
2021-03-10 11:25:07 +0800

    c

--- dqn.py
+++ dqn.py
@@ -99,7 +99,7 @@ class DQN(object):
 dqn = DQN()
 
 print('\nCollecting experience...')
-for i_episode in range(400):
+for i_episode in range(50):
     s = env.reset()
     ep_r = 0
     while True:
