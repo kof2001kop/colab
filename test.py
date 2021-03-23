f03dd280b3d100352e2d1ec98e72cdcfd1c26076
kof2001kop <kof2001kop@163.com>
2021-03-23 14:06:12 +0800

    c

---
+++ test.py
@@ -0,0 +1,17 @@
+from torch import nn
+ 
+class simpleNet(nn.Module):
+    """
+    定义了一个简单的三层全连接神经网络，每一层都是线性的
+    """
+    def __init__(self, in_dim, n_hidden_1, n_hidden_2, out_dim):
+        super(simpleNet, self).__init__()
+        self.layer1 = nn.Linear(in_dim, n_hidden_1)
+        self.layer2 = nn.Linear(n_hidden_1, n_hidden_2)
+        self.layer3 = nn.Linear(n_hidden_2, out_dim)
+ 
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        return x
