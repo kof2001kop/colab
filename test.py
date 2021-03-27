10b995bc1990500ddabf300ec1dd47aa37a2c121
kof2001kop <kof2001kop@163.com>
2021-03-27 13:35:31 +0800

    c

--- test.py
+++ test.py
@@ -12,11 +12,11 @@ one = torch.ones_like(y_t)
 y_tag = torch.where(y_t > 0, one, zero)
 '''
 
-x1_t = torch.normal(2*torch.ones(100,2),1)
-y1_t = torch.zeros(100)
+x1_t = torch.normal(2*torch.ones(10,2),1)
+y1_t = torch.zeros(10)
 
-x2_t = torch.normal(-2*torch.ones(100,2),1)
-y2_t = torch.ones(100)
+x2_t = torch.normal(-2*torch.ones(10,2),1)
+y2_t = torch.ones(10)
 
 x_t = torch.cat((x1_t,x2_t),0)
 y_t = torch.cat((y1_t,y2_t),0)
