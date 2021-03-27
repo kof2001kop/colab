26a4716895267ebe8f6fb27142f8f7869656c796
kof2001kop <kof2001kop@163.com>
2021-03-27 12:25:30 +0800

    c

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import torch
 import torch.nn as nn
 
-x1_t = torch.randn(10);
-x2_t = torch.randn(10);
+#x1_t = torch.randn(10);
+#x2_t = torch.randn(10);
 
-m = 2 * x1_t
-y_t = m + x2_t
+#y_t = 2 * x1_t + x2_t
 
-print (m)
-print (x2_t)
-print (y_t)
+x1_t = torch.normal(2*torch.ones(100,2),1)
+y1_t = torch.zeros(100)
+
+print (y1_t)
