c032cb367a3090ce678abaa142790a9227a30453
kof2001kop <kof2001kop@163.com>
2021-03-27 19:05:26 +0800

    c

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ x2_t = (10 * torch.randn(10000)).ceil();
 x_input = torch.stack([x1_t, x2_t],dim=1)
 
 y_t = x1_t * x2_t
-zero = torch.ones_like(y_t)
+zero = torch.zeros_like(y_t)
 one = torch.ones_like(y_t)
 
 y_tag = torch.where(y_t >= 0, one, zero)
