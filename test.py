6b8160340629a32f89db280f792b9555670b57ce
kof2001kop <kof2001kop@163.com>
2021-03-27 13:29:28 +0800

    c

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
 
-x1_t = torch.randn(10);
-x2_t = torch.randn(10);
+x1_t = torch.randn(100);
+x2_t = torch.randn(100);
 x_input = torch.stack([x1_t, x2_t],dim=1)
 
 y_t = x1_t * x2_t
