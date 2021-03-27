50efa9a7c36c231210c8b347bcce467e9c037404
kof2001kop <kof2001kop@163.com>
2021-03-27 17:33:50 +0800

    c

--- test.py
+++ test.py
@@ -13,7 +13,6 @@ y_tag = torch.where(y_t > 0, one, zero)
 
 #print (x_input)
 #print (y_tag)
-print (net(x_input))
 
 '''
 x1_t = torch.normal(2*torch.ones(10,2),1)
@@ -26,7 +25,7 @@ x_input = torch.cat((x1_t,x2_t),0)
 y_tag = torch.cat((y1_t,y2_t),0)
 '''
 
-'''
+
 net = nn.Sequential(
     nn.Linear(2,5),  # 输入层与第一隐层结点数设置，全连接结构
     torch.nn.Sigmoid(),  # 第一隐层激活函数采用sigmoid
@@ -35,7 +34,9 @@ net = nn.Sequential(
     nn.Linear(5,2),  # 第二隐层与输出层层结点数设置，全连接结构
     nn.Softmax(dim=1) # 由于有两个概率输出，因此对其使用Softmax进行概率归一化
 )
+print (net(x_input))
 
+'''
 # 配置损失函数和优化器
 optimizer = torch.optim.SGD(net.parameters(),lr=0.01) # 优化器使用随机梯度下降，传入网络参数和学习率
 loss_func = torch.nn.CrossEntropyLoss() # 损失函数使用交叉熵损失函数
