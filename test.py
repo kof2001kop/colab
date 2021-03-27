17a59de3d09058ee63ff11154d1577d40efc40d3
kof2001kop <kof2001kop@163.com>
2021-03-27 19:01:21 +0800

    c

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ one = torch.ones_like(y_t)
 
 y_tag = torch.where(y_t >= 0, one, zero)
 
-print (x_input)
-print (y_tag)
+#print (x_input)
+#print (y_tag)
 
 '''
 x1_t = torch.normal(2*torch.ones(10,2),1)
@@ -25,11 +25,11 @@ x_input = torch.cat((x1_t,x2_t),0)
 y_tag = torch.cat((y1_t,y2_t),0)
 '''
 
-'''
+
 net = nn.Sequential(
-    nn.Linear(2,5),  # 输入层与第一隐层结点数设置，全连接结构
+    nn.Linear(2,3),  # 输入层与第一隐层结点数设置，全连接结构
     torch.nn.Sigmoid(),  # 第一隐层激活函数采用sigmoid
-    nn.Linear(5,2),  # 第二隐层与输出层层结点数设置，全连接结构
+    nn.Linear(3,2),  # 第二隐层与输出层层结点数设置，全连接结构
     nn.Softmax(dim=1) # 由于有两个概率输出，因此对其使用Softmax进行概率归一化
 )
 
@@ -57,4 +57,3 @@ for epoch in range(num_epoch):
         print('epoch: {}, loss: {}'.format(epoch, loss.data.item()))
 
 torch.save(net,'colab/net.pkl')
-'''
