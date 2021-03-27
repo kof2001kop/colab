827a5565fbaf407a7f40c686040c098d56c481f6
kof2001kop <kof2001kop@163.com>
2021-03-27 15:03:38 +0800

    c

--- load_pkl.py
+++ load_pkl.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-
+'''
 net = nn.Sequential(
     nn.Linear(2,5),  
     torch.nn.Sigmoid(),  
@@ -8,6 +8,5 @@ net = nn.Sequential(
     torch.nn.Sigmoid(),  
     nn.Linear(5,2),  
     nn.Softmax(dim=1) )
-
-torch.load('colab/net.pkl')
-#net.load_state_dict(torch.load('colab/net.pkl'))
+'''
+net = torch.load('colab/net.pkl')
