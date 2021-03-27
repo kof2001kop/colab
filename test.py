5cd97fc4b70b89b990cd21f379574668bfda7ad9
kof2001kop <kof2001kop@163.com>
2021-03-27 12:50:52 +0800

    c

--- test.py
+++ test.py
@@ -5,9 +5,9 @@ x1_t = torch.randn(10);
 x2_t = torch.randn(10);
 x_t = torch.stack([x1_t, x2_t],dim=1)
 
-y_t = 2 * x1_t + x2_t
+y_t = x1_t * x2_t
 
-print (2 * x1_t)
+print (x1_t)
 print (x2_t)
 print (y_t)
 
