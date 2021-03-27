83b02a4b9a1b610af3f10becb0f70ea8524a3db1
kof2001kop <kof2001kop@163.com>
2021-03-27 13:09:22 +0800

    c

--- test.py
+++ test.py
@@ -3,19 +3,16 @@ import torch.nn as nn
 
 x1_t = torch.randn(10);
 x2_t = torch.randn(10);
-x_t = torch.stack([x1_t, x2_t],dim=1)
+x_input = torch.stack([x1_t, x2_t],dim=1)
 
 y_t = x1_t * x2_t
-
 zero = torch.zeros_like(y_t)
 one = torch.ones_like(y_t)
 
 left = torch.where(y_t > 0, one, zero)
 right = torch.where(y_t > 0, zero, one)
+y_out = torch.stack([left, right],dim=1)
 
-print (x1_t)
-print (x2_t)
-print (y_t)
 print (left)
 print (right)
-
+print (y_out)
