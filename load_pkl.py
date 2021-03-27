03753d306a0192ab7b31e74a52840f960702a7ff
kof2001kop <kof2001kop@163.com>
2021-03-27 18:47:06 +0800

    c

--- load_pkl.py
+++ load_pkl.py
@@ -5,5 +5,11 @@ x = torch.tensor([[2., 3.]])
 
 net = torch.load('colab/net.pkl')
 result = net(x)
-print (result[0][0])
-print (result[0][1])
+
+if (result[0][0] >= 0.5)
+	print ("负")
+else
+	print ("正")
+
+#print (result[0][0])
+#print (result[0][1])
