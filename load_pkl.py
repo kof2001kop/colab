8ac80f747901242b9fe8e1da8a233be1d76d2b60
kof2001kop <kof2001kop@163.com>
2021-03-27 14:58:10 +0800

    c

---
+++ load_pkl.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+net = nn.Sequential(
+    nn.Linear(2,5),  
+    torch.nn.Sigmoid(),  
+    nn.Linear(5,5),  
+    torch.nn.Sigmoid(),  
+    nn.Linear(5,2),  
+    nn.Softmax(dim=1) 
+    )
+
+net.load_state_dict(torch.load('net_parameter.pkl')
