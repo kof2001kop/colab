e0029b5c03eacf634b7863855c16e59d064c1413
kof2001kop <kof2001kop@163.com>
2021-03-27 15:06:03 +0800

    c

--- load_pkl.py
+++ load_pkl.py
@@ -1,12 +1,7 @@
 import torch
 import torch.nn as nn
-'''
-net = nn.Sequential(
-    nn.Linear(2,5),  
-    torch.nn.Sigmoid(),  
-    nn.Linear(5,5),  
-    torch.nn.Sigmoid(),  
-    nn.Linear(5,2),  
-    nn.Softmax(dim=1) )
-'''
+
+x = torch.tensor([[2, 3]])
+
 net = torch.load('colab/net.pkl')
+print (net(x))
